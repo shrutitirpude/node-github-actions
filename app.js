2cf3e7dbcb99fd6782b5b9db18decc9c5ca1a2e1
shruti-tirpude <149510402+shrutitirpude@users.noreply.github.com>
2025-09-24 15:30:10 -0500

    change the message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! Sample Node App for GitHub Actions Small Learning Pod");
 });
 
 app.get("/health", (req, res) => {
